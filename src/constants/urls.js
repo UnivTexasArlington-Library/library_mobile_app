444e8ae46cc2f8786d4a09e71e3a0d151b803f00
Hunter <erik.hunter@uta.edu>
2023-08-24 12:57:42 -0500

    added gitignore file

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,11 +1,6 @@
-import {
-  REACT_APP_URL,
-  REACT_APP_URL_TEST,
-  REACT_APP_NEWS_AND_EVENT_URL,
-} from "@env";
 //Main UTA Library URL
-export const URL = REACT_APP_URL;
+export const URL = process.env.EXPO_PUBLIC_URL;
 //Test UTA Library URL
-export const URL_TEST = REACT_APP_URL_TEST;
+export const URL_TEST = process.env.EXPO_PUBLIC_URL_TEST;
 //The following Drupal JSON:API URL is used for gathering data related to news events.
-export const newsAndEventsURL = REACT_APP_NEWS_AND_EVENT_URL;
+export const newsAndEventsURL = process.env.EXPO_PUBLIC_NEWS_AND_EVENT_URL;
